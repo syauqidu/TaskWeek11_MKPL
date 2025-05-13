3eea55084a461fec71786efdf8c4b727815be380
Syauqi <syauqi.d.u@gmail.com>
2025-05-13 19:42:40 +0700

    Pembaruan ngaco

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -13,11 +13,11 @@ public class Counter {
 	}
 	
 	public void increment() {
-		count++;
+		count += 2;
 	}
 	
 	public void decrement() {
-		count--;
+		count -= 2;
 	}
 	
 	public int getCount() {
