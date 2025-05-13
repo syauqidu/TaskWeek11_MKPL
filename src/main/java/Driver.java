9c8910d652191e4d78edd4bf4f045e96d4a0eb92
Syauqi <syauqi.d.u@gmail.com>
2025-05-13 19:39:48 +0700

    Pemabruan yang sukses

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,15 +6,17 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Count aWAL:" + counter.getCount());
 		
 		counter.increment();
+                counter.increment();
 		
 		System.out.println("Count:" + counter.getCount());
 		
 		counter.decrement();
+                counter.decrement();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Count BARU BANGET:" + counter.getCount());
 		
 	}
 
